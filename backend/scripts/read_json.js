2678b6956ccb3eb7200d551cdf8a14c0537e136f
Anish-100 <anish10707@gmail.com>
2026-09-03 19:03:07 -0700

    Added json file containing all relevant buildings and their locations, along with codenames that is used to refer to them.

---
+++ backend/scripts/read_json.js
@@ -0,0 +1,78 @@
+import fs from 'node:fs/promises';
+import path from 'node:path'
+
+
+const dataDir = path.join(import.meta.dirname,'..','data')
+
+const days_code = new Map([['M',1],['Tu',2],['W',3],['Th',4],['F',5]])
+
+async function uci_meetings_json(quarter, year){
+    const raw = await fs.readFile(path.join(dataDir,`${quarter}_${year}.json`), {encoding:'utf8'});
+    const year_data = JSON.parse(raw)
+    let values = []
+    for (let school of year_data.data.schools){
+    for (let dep of school.departments){
+        for (let courses of dep.courses){
+            for (let sections of courses.sections){
+                for (let meeting of sections.meetings){
+                    if (meeting.timeIsTBA)continue
+                    if (meeting.bldg == "TBA" || meeting.bldg == 'ON LINE') continue
+                    const split_days = splitDays(meeting.days)
+                    for (let day of split_days){
+                        for (let bldg of meeting.bldg){
+                            values.push({
+                                courseId: courses.courseId,
+                                startTime: meeting.startTime,
+                                endTime: meeting.endTime,
+                                bldg,
+                                day,
+                            })
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+ return values;
+
+}
+
+function splitDays(days){
+    let res = []
+    let len_s = days.length
+    let i = 0
+    while (i < len_s){
+        if (days_code.has(days[i])){
+            res.push(days_code.get(days[i]))
+        }
+        if (days[i] == 'T'){
+            res.push(days_code.get(days[i] +days[i+1]))
+            i++;
+        }
+        i++;
+    }
+    return res
+
+}
+
+async function uci_buildings_json(){
+    const raw = await (fs.readFile(path.join(dataDir, 'buildings.json'), {encoding:'utf8'}))
+    const buildings_data = JSON.parse(raw)
+    return buildings_data;
+
+}
+export function getAnteaterData(){
+    return uci_meetings_json('Fall', 2026);
+}
+
+export async function getBuildingsData(){
+    return await uci_buildings_json()
+}
+getBuildingsData()
+// After meetings
+//.bldg, if bldg is "TBA" or "", skip 
+//.days
+//.startTime , under starTime, we will take "hour", "minute"
+//.endTime
